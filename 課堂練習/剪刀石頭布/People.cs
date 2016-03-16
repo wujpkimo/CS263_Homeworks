53bbb97164cb8d6241ac5b35370aa56ec8be8612
Jason.Wu <wujpkimo@gmail.com>
2016-03-17 00:54:07 +0800

    隨堂課題-剪刀石頭布(雙人版)

---
+++ 課堂練習/剪刀石頭布/People.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 剪刀石頭布
+{
+    internal class People
+    {
+        public int H;
+        public string Q;
+        public string Result;
+
+        public string ToQ()
+        {
+            switch (this.H)
+            {
+                case 0:
+                    this.Q = "剪刀";
+                    break;
+
+                case 1:
+                    this.Q = "石頭";
+                    break;
+
+                case 2:
+                    this.Q = "布";
+                    break;
+            }
+            return this.Q;
+        }
+    }
+}
---
+++ 課堂練習/剪刀石頭布/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 剪刀石頭布
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            People P1 = new People
+            {
+                H = rnd.Next(0, 3)
+            };
+            People P2 = new People
+            { H = rnd.Next(0, 3) };
+
+            if (P1.H == P2.H)
+            {
+                P1.Result = P2.Result = "平手";
+            }
+            else if ((P1.H == 0 && P2.H == 1) ||
+                     (P1.H == 1 && P2.H == 2) ||
+                     (P1.H == 2 && P2.H == 0))
+            {
+                P1.Result = "輸";
+                P2.Result = "贏";
+            }
+            else if ((P1.H == 0 && P2.H == 2) ||
+                     (P1.H == 1 && P2.H == 0) ||
+                     (P1.H == 2 && P2.H == 1))
+            {
+                P1.Result = "贏";
+                P2.Result = "輸";
+            }
+            Console.WriteLine("P1出{0}:{1}\n", P1.ToQ(), P1.Result);
+            Console.WriteLine("P2出{0}:{1}\n", P2.ToQ(), P2.Result);
+        }
+    }
+}
